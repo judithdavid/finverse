a10081bb6fd0728f8501404f2b1da314a84a993d
judithdavid <davidjudith2001@gmail.com>
2026-07-18 12:33:07 +0530

    Configure SQLAlchemy database connection

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,3 +6,5 @@ load_dotenv()
 APP_NAME = os.getenv("APP_NAME", "FinVerse API")
 APP_VERSION = os.getenv("APP_VERSION", "0.1.0")
 DEBUG = os.getenv("DEBUG", "False") == "True"
+
+DATABASE_URL = os.getenv("DATABASE_URL")
