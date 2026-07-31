566c6645c2ee0eaad4a5e2b1674b4a445079366a
judithdavid <davidjudith2001@gmail.com>
2026-07-31 14:15:04 +0530

    Add savings goal model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -3,6 +3,7 @@ from .wallet import Wallet
 from .transaction import Transaction
 from .category import Category
 from .budget import Budget
+from .savings_goal import SavingsGoal
 
 __all__ = ["User"]
 
